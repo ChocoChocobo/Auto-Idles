9a39867fe8858ed05e7534845710901e97ac7e57
Manticore <78116604+ChocoChocobo@users.noreply.github.com>
2024-08-16 19:10:07 +0300

    feat: random instantiation idols

--- Assets/_Scripts/Managers/BattleInitializer.cs
+++ Assets/_Scripts/Managers/BattleInitializer.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
@@ -12,26 +12,57 @@ public class BattleInitializer : StaticInstance<BattleInitializer>
 
     public bool battleStarted = false;
 
+    [SerializeField] private GameObject gridPlayerParent;
+    [SerializeField] private GameObject gridEnemyParent;
+
+    [SerializeField] private int idolsAmount;
+
     private void Start()
     {
-        SpawnCharacters();
+        SpawnPlayerIdols();
+        SpawnEnemyIdols();
     }
 
-    private void SpawnCharacters()
-    {
-        // Haruka test spawn as players idol
-        var harukaScriptable = ResourceSystem.Instance.GetIdolByName("Haruka");
-        var spawned = Instantiate(harukaScriptable.prefab, spawnPoints[0].position, Quaternion.identity, spawnPoints[0]);
-        spawned.name = "Haruka";
-        var stats = harukaScriptable.BaseStats;
-        spawned.SetStats(stats);
+    private void SpawnPlayerIdols()
+    {        
+        GameObject[] playerTiles = new GameObject[gridPlayerParent.transform.childCount];
+        for (int i = 0; i < playerTiles.Length; i++)
+        {
+            playerTiles[i] = gridPlayerParent.transform.GetChild(i).gameObject;
+        }
 
-        // Iori test spawn as enemy
-        var ioriScriptable = ResourceSystem.Instance.GetIdolByName("Iori");
-        var spawned1 = Instantiate(ioriScriptable.prefab, spawnPoints[1].position, Quaternion.identity, spawnPoints[1]);
-        spawned1.name = "Iori";
-        var stats1 = ioriScriptable.BaseStats;
-        spawned1.SetStats(stats1);
+        for (int i = 0; i < idolsAmount; i++)
+        {
+            // TODO: add a check for if a tile is occupied already. Try searching for not occupied tile
+            GameObject randomTile = playerTiles[Random.Range(1, playerTiles.Length)];
+            // Haruka test spawn as players idol
+            var harukaScriptable = ResourceSystem.Instance.GetIdolByName("Haruka");
+            var spawned = Instantiate(harukaScriptable.prefab, randomTile.transform.position, Quaternion.identity, randomTile.transform);
+            spawned.name = "Haruka";
+            var stats = harukaScriptable.BaseStats;
+            spawned.SetStats(stats);
+        }
+
+    }
+
+    private void SpawnEnemyIdols() // TODO: Remake so that it fetches the active player idol roster through scenes
+    {
+        GameObject[] enemyTiles = new GameObject[gridEnemyParent.transform.childCount];
+        for (int i = 0; i < enemyTiles.Length; i++)
+        {
+            enemyTiles[i] = gridEnemyParent.transform.GetChild(i).gameObject;
+        }
+
+        for (int i = 0; i < idolsAmount; i++)
+        {
+            GameObject randomTile = enemyTiles[Random.Range(1, enemyTiles.Length)];
+            // Iori test spawn as enemy
+            var ioriScriptable = ResourceSystem.Instance.GetIdolByName("Iori");
+            var spawned = Instantiate(ioriScriptable.prefab, randomTile.transform.position, Quaternion.identity, randomTile.transform);
+            spawned.name = "Iori";
+            var stats = ioriScriptable.BaseStats;
+            spawned.SetStats(stats);
+        }
     }
 
     public void StartBattle()
--- Assets/_Scripts/Managers/GridManager/GridManager.cs
+++ Assets/_Scripts/Managers/GridManager/GridManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
+public class GridManager : StaticInstance<GridManager>
 {
+    [Header("Grid params")]
     [SerializeField] private int gridWidth, gridHeight;
     [SerializeField] private int playerGridStartPos, enemyGridStartPos;
+    [Header("Attachables")]
     [SerializeField] private Tile tilePrefab;
     [SerializeField] private EnemyTile enemyTilePrefab;
     [SerializeField] private Transform cam;
+    [SerializeField] private Transform gridPlayerParent;
+    [SerializeField] private Transform gridEnemyParent;
 
     private void Start()
     {
@@ -22,7 +26,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < gridHeight; y++)
             {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(playerGridStartPos + x, y), Quaternion.identity, transform);
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(playerGridStartPos + x, y), Quaternion.identity, gridPlayerParent);
                 spawnedTile.name = $"Tile {x}-{y}";
             }
         }
@@ -35,7 +39,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < gridHeight; y++)
             {
-                var spawnedTile = Instantiate(enemyTilePrefab, new Vector3(enemyGridStartPos + x, y), Quaternion.identity, transform);
+                var spawnedTile = Instantiate(enemyTilePrefab, new Vector3(enemyGridStartPos + x, y), Quaternion.identity, gridEnemyParent);
                 spawnedTile.name = $"Enemy tile {x}-{y}";
             }
         }
