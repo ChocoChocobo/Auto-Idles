d75a8fd01e70fcc181caad7783b3c98f6e89351b
Manticore <78116604+ChocoChocobo@users.noreply.github.com>
2024-10-21 13:10:01 +0300

    edit: remade timer with a tiredness slider

---
+++ Assets/_Scripts/Managers/TirednessMeter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// TimerManager class is responsible for timer logic at Night scene.
+/// </summary>
+public class TirednessMeter : StaticInstance<TirednessMeter>
+{
+    //[SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private Slider tirednessSlider;
+    [SerializeField] private float remainingTime; // In minutes
+    // public static event Action<bool> FightEnded;
+    // FightEnded?.Invoke();
+
+    protected override void Awake()
+    {
+        base.Awake();
+        tirednessSlider.value = remainingTime;
+    }
+
+    private void Update()
+    {
+        if (remainingTime > 0 && BattleInitializer.Instance.battleStarted)
+        {
+            remainingTime -= Time.deltaTime;
+            tirednessSlider.value = remainingTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            //timerText.color = Color.red;
+            Debug.LogWarning("Time is up! idols head back to their studios!");
+        }
+        /*int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);*/
+    }
+}
